778a00e7771fad6e78c75bebd1b5e0336287dcc8
sa <SasaIcemore@163.com>
2019-03-13 10:49:16 +0800

    添加传递参数的处理

--- GalaxyAPI.V01/Controllers/galaxy_api/GalaxyController.cs
+++ GalaxyAPI.V01/Controllers/galaxy_api/GalaxyController.cs
@@ -1,4 +1,5 @@
 ﻿using GalaxyApi;
+using GalaxyApi.Model;
 using GalaxyAPI.V01.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Cors;
@@ -6,6 +7,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Net.Http;
 
@@ -17,17 +19,31 @@ namespace GalaxyAPI.V01.Controllers.galaxy_api
     public class GalaxyController : Controller
     {
         APIDataHelper dataHelper = new APIDataHelper();
-
-        [HttpGet("{apiCode}/{pCount}/{pNum}"), FormatFilter]
-        public IActionResult API(string apiCode, int pCount, int pNum)
+        
+        [HttpPost]
+        public IActionResult API(string apiCode, int pCount=1, int pNum=1)
         {
+            //获取参数,json串
+            List<APIParamsFilter> paralist = null;
+            try
+            {
+                string paramStr = HttpContext.Request.Form["filter"];
+                paralist = JsonConvert.DeserializeObject<List<APIParamsFilter>>(paramStr);
+            }
+            catch
+            {
+                paralist = null;
+            }
+            
+
             //获取令牌，取得角色id
             string auth = HttpContext.Request.Headers["Authorization"];
             int role_id = 0;
             using (HttpClient client = new HttpClient())
             {
                 client.BaseAddress = new Uri(MyConfig.ConfigManager.AUTHORITY);
-                client.SetBearerToken(auth.Substring(7));
+                //client.SetBearerToken(auth.Substring(7));
+                client.DefaultRequestHeaders.Add("Authorization", auth);
                 var t = client.GetAsync("/connect/userinfo");
                 t.Wait();
                 var result = t.Result;
@@ -40,7 +56,7 @@ namespace GalaxyAPI.V01.Controllers.galaxy_api
             if (canUse)
             {
                 APIDataHelper datahelper = new APIDataHelper();
-                DataTable tbl = datahelper.GetGuideAPIByCode(apiCode, pCount, pNum);
+                DataTable tbl = datahelper.GetGuideAPIByCode(apiCode, pCount, pNum, paralist);
                 string jsonStr = JsonConvert.SerializeObject(tbl);
                 return Content(jsonStr);
             } 
