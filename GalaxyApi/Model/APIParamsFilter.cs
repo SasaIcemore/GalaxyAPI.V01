68da74aec7ba4634a3b3493694c1f6713b8abb8c
sa <SasaIcemore@163.com>
2019-03-13 17:56:57 +0800

    。。

---
+++ GalaxyApi/Model/APIParamsFilter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GalaxyApi.Model
+{
+    public class APIParamsFilter
+    {
+        //拼接逻辑 and/or
+        public string Logic { get; set; }
+        //参数名称
+        public string ParamName { get; set; }
+        //操作符
+        public string Operation { get; set; }
+        //public string ValueType { get; set; }
+        //参数值
+        public string Value { get; set; }
+    }
+}
